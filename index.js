8559b27052d5be745ac3df6eb1cb3ad18a2d4fed
Ibnul Ferdous <ibnul04@gmail.com>
2021-11-12 21:47:06 +0600

    Third days end

--- index.js
+++ index.js
@@ -21,15 +21,59 @@ async function run() {
         await client.connect();
 
         const database = client.db("vidflyDB");
+        const dronesCollection = database.collection("drones");
         const usersCollections = database.collection("users");
+        const ordersCollection = database.collection("orders");
 
-        // Registed users- POST API
+
+        // Drones- GET API for all
+        app.get('/drones', async (req, res) => {
+            const cursor = dronesCollection.find({});
+            const drones = await cursor.toArray();
+            res.send(drones);
+        })
+
+        // Drones- GET API for one drone
+        app.get('/drones/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const drone = await dronesCollection.findOne(query);
+            res.json(drone);
+        })
+
+        // Drones- POST API
+        app.post('/drones', async (req, res) => {
+            const drones = req.body;
+            const result = await dronesCollection.insertOne(drones);
+
+            res.json(result);
+        });
+
+
+
+        // ------------------------------------------------------------------
+
+        // Users- POST API
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollections.insertOne(user);
 
             res.json(result);
         });
+
+
+        // ------------------------------------------------------------------
+
+        // Orders- POST API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+
+            const result = await ordersCollection.insertOne(order);
+
+            res.json(result);
+        });
+
+
 
         
 
