0b4a0d37d6b117e622ff088190100cd93d91060c
Ibnul Ferdous <ibnul04@gmail.com>
2021-11-13 23:22:48 +0600

    Preparing for heroku server

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function run() {
         const dronesCollection = database.collection("drones");
         const usersCollections = database.collection("users");
         const ordersCollection = database.collection("orders");
+        const reviewsCollection = database.collection("reviews");
 
 
         // Drones- GET API for all
@@ -49,9 +50,26 @@ async function run() {
             res.json(result);
         });
 
+        // Drones- DELETE API
+        app.delete('/drones/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await dronesCollection.deleteOne(query);
+
+            res.json(result);
+        })
+
+
 
 
         // ------------------------------------------------------------------
+
+        // Users- GET API for all
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollections.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        })
 
         // Users- POST API
         app.post('/users', async (req, res) => {
@@ -61,8 +79,42 @@ async function run() {
             res.json(result);
         });
 
+        // Users- UPDATE API
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: false };
+
+            const updateStatus = {
+                $set: {
+                    role: `admin`
+                },
+            };
+
+            const result = await usersCollections.updateOne(filter, updateStatus, options);
+
+            res.json(result);
+        })
+
+        // User- POST API to find data by email
+        app.post('/users/by-email', async (req, res) => {
+            const userEmail = req.body;
+            const query = { email: { $in: userEmail } };
+            const user = await usersCollections.find(query).toArray();
+            res.json(user);
+        })
+
+
+
 
         // ------------------------------------------------------------------
+
+        // Orders- GET API for all
+        app.get('/orders', async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
 
         // Orders- POST API
         app.post('/orders', async (req, res) => {
@@ -81,6 +133,23 @@ async function run() {
             res.json(ordersByUser);
         })
 
+        // Orders- UPDATE API
+        app.put('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: false };
+
+            const updateStatus = {
+                $set: {
+                    orderStatus: `shipped`
+                },
+            };
+
+            const result = await ordersCollection.updateOne(filter, updateStatus, options);
+
+            res.json(result);
+        })
+
         // Orders- DELETE API
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
@@ -88,6 +157,26 @@ async function run() {
             const result = await ordersCollection.deleteOne(query);
 
             res.json(result);
+        })
+
+
+
+
+        // ------------------------------------------------------------------
+
+        // Reviews- POST API
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+
+            res.json(result);
+        });
+
+        // Reviews- GET API for all
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
         })
 
 
